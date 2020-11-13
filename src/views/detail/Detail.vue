6c6197d40e22049f137c5cf55d734d10ac7f2da1
GideonSenku <gentlewind2391@gmail.com>
2020-11-13 16:21:26 +0800

    完成[detail]的图片信息和参数数据显示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="detail">
     <detail-nav-bar class="detail-nav" />
-    <scroll class="scroll-height">
+    <scroll class="scroll-height" ref="scroll">
       <detail-swiper :images="topImages"/>
       <detail-base-info :goods="goods"/>
       <detail-shop-info :shop="shop"/>
+      <detail-images-info :images-info="detailInfo" @imgLoad="imgLoad" />
+      <detail-param-info :param-info="paramsInfo"/>
     </scroll>
   </div>
 </template>
@@ -14,10 +16,11 @@ import DetailNavBar from './childrenComps/DetailNavBar'
 import DetailSwiper from './childrenComps/DetailSwiper'
 import DetailBaseInfo from './childrenComps/DetailBaseInfo'
 import DetailShopInfo from './childrenComps/DetailShopInfo'
-
+import DetailImagesInfo from './childrenComps/DetailImagesInfo.vue'
+import DetailParamInfo from './childrenComps/DetailParamInfo.vue'
 import Scroll from 'components/common/scroll/Scroll'
 
-import { getDetails, Goods, Shop } from 'network/detail'
+import { getDetails, Goods, Shop, GoodsParams } from 'network/detail'
 
 export default {
   name: 'Detail',
@@ -26,24 +29,41 @@ export default {
       iid: null,
       topImages: [],
       goods: {},
-      shop: {}
+      shop: {},
+      detailInfo: {},
+      paramsInfo: {}
     }
   },
   created() {
     this.iid = this.$route.query.iid
+    // 根据iid请求详情数据
     getDetails(this.iid).then((res) => {
       const data = res.result
+      // 1. 获取顶部轮播图数据
       this.topImages = data.itemInfo.topImages
+      // 2. 获取商品信息
       this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      // 3. 创建店铺信息对象
       this.shop = new Shop(data.shopInfo)
+      // 4. 保存商品的详情数据
+      this.detailInfo = data.detailInfo
+      // 5. 保存商品的参数数据
+      this.paramsInfo = new GoodsParams(data.itemParams.info, data.itemParams.rule || {})
     })
+  },
+  methods: {
+    imgLoad() {
+      this.$refs.scroll.refresh()
+    }
   },
   components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
-    Scroll
+    DetailImagesInfo,
+    DetailParamInfo,
+    Scroll,
   }
 }
 </script>
